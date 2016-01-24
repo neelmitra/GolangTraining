f60cd97bb616b8e9d7587269de187e9a839bba40
neelmitra <raja.mtr@gmail.com>
2016-01-24 13:13:57 -0500

    Test commit

---
+++ 01_getting-started/02_numeral-systems/04_loop/looputf.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Printing the Ascii, Binary, Hex and UTF codes from A to Z , in small and caps")
+	for i := 65; i < 127; i++ {
+		fmt.Printf("%d \t %b \t %#x \t %q \n", i, i, i, i)
+	}
+}
