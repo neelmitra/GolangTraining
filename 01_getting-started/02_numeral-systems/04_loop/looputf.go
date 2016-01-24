1c7b310d9167fd7706ca0d4fadf2f6fe4077c114
neelmitra <raja.mtr@gmail.com>
2016-01-24 13:19:14 -0500

    additional changes

--- 01_getting-started/02_numeral-systems/04_loop/looputf.go
+++ 01_getting-started/02_numeral-systems/04_loop/looputf.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func main() {
 	fmt.Println("Printing the Ascii, Binary, Hex and UTF codes from A to Z , in small and caps")
+	fmt.Println("Testing Github push")
 	for i := 65; i < 127; i++ {
 		fmt.Printf("%d \t %b \t %#x \t %q \n", i, i, i, i)
 	}
