634f438e175866048e7facebdd1d129c80af264c
neelmitra <raja.mtr@gmail.com>
2016-01-24 20:48:22 -0500

    update j

--- 03_variables/03_less-emphasis/08_exercise_your-name/01_oneSolution/myNameVar.go
+++ 03_variables/03_less-emphasis/08_exercise_your-name/01_oneSolution/myNameVar.go
@@ -2,10 +2,12 @@ package main
 
 import "fmt"
 
-var name = "Todd"
+var name = "Mishtu"
 var nm string = "Neel"
 
 func main() {
 	fmt.Println("Hello ", name)
 	fmt.Printf("This is %s \n", nm)
+	nme := "Jishu"
+	fmt.Println("This is ", nme)
 }
