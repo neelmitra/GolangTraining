5ae9ba05ca9bfee4f80212e42eff1e1a9231521f
neelmitra <raja.mtr@gmail.com>
2016-02-01 20:19:06 -0500

    changes

--- 10_for-loop/07_rune-loop_UTF8/01/main.go
+++ 10_for-loop/07_rune-loop_UTF8/01/main.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+//conversion of integer to string , then conversion of string to rune (bytes)
+
 func main() {
 	for i := 50; i <= 140; i++ {
 		fmt.Println(i, " - ", string(i), " - ", []byte(string(i)))
