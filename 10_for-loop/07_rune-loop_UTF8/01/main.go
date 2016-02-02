26a00b8d7b0014d71e85604fe84bb6fb27bb4e81
neelmitra <raja.mtr@gmail.com>
2016-02-01 21:11:07 -0500

    changes

--- 10_for-loop/07_rune-loop_UTF8/01/main.go
+++ 10_for-loop/07_rune-loop_UTF8/01/main.go
@@ -8,7 +8,13 @@ func main() {
 	for i := 50; i <= 140; i++ {
 		fmt.Println(i, " - ", string(i), " - ", []byte(string(i)))
 	}
-	foo := "a"
+	foo := "a" // string
+	boo := 'b' //rune
+	doo := `\t` //raw string literal for interpreting characters as it is like \r, \t
 	fmt.Printf("This is %s ", foo)
-	fmt.Printf("%T \n", foo)
+	fmt.Printf("of type %T \n", foo)
+	fmt.Printf("This is %v ", boo)
+	fmt.Printf("of type %T \n", boo)
+	fmt.Printf("This is %v ", doo)
+	fmt.Printf("of type %T \n", doo)
 }
